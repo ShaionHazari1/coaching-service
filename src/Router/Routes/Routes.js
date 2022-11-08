b68c7f7fa6d993bc6fdb0029d52a2668125c9b7a
unknown <rokihazari@gmail.com>
2022-11-08 07:57:11 +0300

    ROuter and Layout setup

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,13 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+
+        ]
+    }
+]);
+export default router;
